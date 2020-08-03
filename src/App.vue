56e26d82a153836684a81e31edad6d0cd8ea763b
Hlib Omelchenko <omelchenkohlib@gmail.com>
2020-08-04 01:32:30 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,172 @@
+<template>
+  <div id="app">
+    <button v-if="status === 'Waiting'" class="strat-button" @click="randomizeStrat">
+      Get Pro Strat
+    </button>
+    <strat v-else :strat="currentStrat" :status="status" />
+    <div
+      :class="[`reset-button`, { show: status === 'Completed' }]" @click="resetState">
+      <img src="./assets/refresh-icon.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+import Strat from '@/components/strat.vue';
+
+export default {
+  name: 'App',
+  components: { Strat },
+  data() {
+    return {
+      strats: [
+        'Pistols Only',
+        'SMG Only',
+        'DMR Only',
+        'Shotgun Only',
+        'Shields Only',
+        'LMG Only',
+        'Hipfire Only',
+        'Dropshot Only',
+        'Prone(Def Only)',
+        'Crouch Only',
+        'Burst Mode',
+        'One-Tap Mode',
+        'Sniper Rifle',
+        'One-Bullet Mag',
+        'Teamkill',
+        'Suicide',
+      ],
+      currentStrat: '',
+      status: 'Waiting',
+    };
+  },
+  methods: {
+    randomizeStrat() {
+      if (this.status === 'Waiting' || this.status === 'Completed') {
+        this.currentStrat = '';
+        this.status = 'Randomizing';
+        const interval = setInterval(() => {
+          this.currentStrat = this.strats[this.getRandomIndex()];
+        }, 150);
+
+        setTimeout(() => {
+          this.status = 'Completed';
+          clearInterval(interval);
+        }, 5000);
+      }
+    },
+    resetState() {
+      this.currentStrat = '';
+      this.status = 'Waiting';
+    },
+    getRandomIndex() {
+      return Math.floor(Math.random() * (this.strats.length));
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  font-family: 'Recursive', sans-serif;
+  color: white;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  background: #56575C;
+
+}
+
+.strat-button {
+  position: relative;
+  border: 2px solid #000;
+  background: #56575C;
+  font-size: 40px;
+  overflow: hidden;
+
+  color: inherit;
+  cursor: pointer;
+
+  padding: 10px 20px;
+  transition: transform .2s;
+
+  animation: appear 1s forwards;
+
+  &:active {
+    transform: scale(0.9) translateY(10px);
+  }
+
+  &::after, &::before {
+    position: absolute;
+    content: '';
+    height: 50%;
+    left: 0;
+    right: 0;
+
+    background: #000;
+    mix-blend-mode: overlay;
+    transition: transform .5s;
+  }
+
+  &::before {
+    top: -50%;
+  }
+
+  &::after {
+    top: 100%;
+  }
+
+  &:hover {
+    &::before {
+      transform: translateY(100%);
+    }
+
+    &::after {
+      transform: translateY(-100%);
+    }
+  }
+}
+
+.reset-button {
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  top: -50px;
+  border-radius: 100%;
+  padding: 5px;
+  cursor: pointer;
+  background: #fff;
+  transition: transform .5s ease-in-out;
+
+  &:hover {
+    transform: translateY(65px) rotate(420deg) !important;
+  }
+
+  &.show {
+    transform: translateY(65px) rotate(0);
+  }
+}
+
+@keyframes appear {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/components/strat.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="strat-wrapper">
+    <div :class="[`pro-strat`, {'strat-selected': status === 'Completed'}]">
+      {{ strat }}
+      <span class="border border-left"></span>
+      <span class="border border-top"></span>
+      <span class="border border-right"></span>
+      <span class="border border-bottom"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    strat: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.strat-wrapper {
+  animation: show-strat 5s forwards;
+  opacity: .3;
+  transform: scale(.7);
+
+  display: flex;
+  flex-direction: column;
+}
+
+.pro-strat {
+  font-size: 40px;
+  transition: all 1s;
+  position: relative;
+  padding: 10px 20px;
+  overflow: hidden;
+
+  &::after, &::before {
+    position: absolute;
+    content: '';
+    top: 0;
+    bottom: 0;
+
+    background: #000;
+    mix-blend-mode: overlay;
+    transition: transform 1s;
+    width: 50%;
+  }
+
+  &::before {
+    left: 50%;
+  }
+
+  &::after {
+    right: 50%;
+  }
+}
+
+.strat-selected {
+  transform: scale(1.2);
+
+  &::after {
+    transform: translateX(-101%);
+  }
+
+  &::before {
+    transform: translateX(101%);
+  }
+
+  & .border {
+    &-left {
+      transform: translateY(-100%);
+    }
+
+    &-top{
+      transform: translateX(100%);
+    }
+
+    &-right {
+      transform: translateY(100%);
+    }
+
+    &-bottom {
+      transform: translateX(-100%);
+    }
+  }
+}
+
+.border {
+  position: absolute;
+  content: '';
+  background: #fff;
+  z-index: -1;
+
+  transition: transform 1s 1s;
+
+  &-left {
+    left: 0;
+    bottom: -100%;
+  }
+
+  &-top {
+    top: 0;
+    left: -100%;
+  }
+
+  &-right {
+    top: -100%;
+    right: 0;
+  }
+
+  &-bottom {
+    right: -100%;
+    bottom: 0;
+  }
+
+  &-left,
+  &-right {
+    width: 2px;
+    height: 100%;
+  }
+
+  &-top,
+  &-bottom {
+    height: 2px;
+    width: 100%;
+  }
+}
+
+@keyframes show-strat {
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+</style>
