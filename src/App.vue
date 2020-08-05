f8abf566452efa888f876eeba6fa36c9b44b6c2a
Hlib Omelchenko <omelchenkohlib@gmail.com>
2020-08-05 13:31:50 +0300

    Change background image

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,10 @@ body {
   width: 100vw;
   height: 100vh;
 
-  background: url("./assets/background.jpg") center no-repeat;
+  background-image: url("./assets/test.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
 .strat-button {
